29a3e8460dbb6092a71b3711dc64f42484d1e16e
Bernardo801125 <sandovalbernardo3@gmail.com>
2025-07-31 13:49:19 -0600

    Crear config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+API_KEY = `eRpb2zYW4Yj1a7paVrJTbhYTUJ8pSxaSz65AXj9aekjVkM8EFwY89ARJufyoc4am`
+API_SECRET = `UoFtJuJyvY02j217dV4TMmYSHda5YznOjQZlQxpHLJUVkC91E9G1lv3RGVXzVHyH`
+
+BASE_URL = 'https://testnet.binance.vision'
