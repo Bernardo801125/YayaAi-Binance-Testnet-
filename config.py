7e9ad36968349cdff362e4dcaa71604f5b19829e
Bernardo801125 <sandovalbernardo3@gmail.com>
2025-07-31 22:48:24 -0600

    Actualizar config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 API_KEY = 'eRpb2zYW4Yj1a7paVrJTbhYTUJ8pSxaSz65AXj9aekjVkM8EFwY89ARJufyoc4am'
 API_SECRET = 'UoFtJuJyvY02j217dV4TMmYSHda5YznOjQZlQxpHLJUVkC91E9G1lv3RGVXzVHyH'
 
-BASE_URL = https://testnet.binance.vision
+BASE_URL = 'https://testnet.binance.vision'
